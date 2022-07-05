fa22a97e9ac061845b008b8c69243d2c30d09078
Mohammed Sadiq Zakariah <68265542+SadiqZak@users.noreply.github.com>
2022-07-05 12:01:45 +0530

    Added liked feat and added responsiveness

--- src/pages/LikedPosts/LikedPosts.jsx
+++ src/pages/LikedPosts/LikedPosts.jsx
@@ -1,15 +1,46 @@
 import React, {useState} from 'react'
+import { useSelector } from 'react-redux'
 import Header from '../../Components/Header/Header'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import { TextCard } from '../../Components/Cards/TextCard/TextCard'
 
 export const LikedPosts = () => {
   const [sidebarState, setSidebarState] = useState("Liked Posts")
+  const {user}= useSelector((store)=>store.authenticate)
+  const {posts} = useSelector((store)=>store.timeline)
+  
+  let feedPosts = posts.filter((post)=>post.likes.likedBy.find((liked)=>liked.username===user.username)?.username===user.username)
   return (
     <div className="App">
         <Header/>
         <div className='App-body'>
         <Sidebar  sidebarState={sidebarState} setSidebarState={setSidebarState}/>
-    <div className='middle-container'>...LikedPosts</div>
+        <div className='middle-container'>
+      <div className="middle-child">
+        <div className="middle-child-wrapper">
+          {
+            feedPosts.map((post)=>(
+                <TextCard
+                key={post._id}
+                firstName={post.firstName}
+                lastName={post.lastName}
+                likes={post.likes}
+                postId={post._id}
+                comments={post.comments}
+                username={post.username}
+                content={post.content}
+                date={post.createdAt}
+          />
+           
+             
+            ))
+          }
+       
+      
+        </div>
+        
+      </div>
+    </div>
     </div>
     </div>
   )
--- src/pages/Profile/Component/ProfileChild.jsx
+++ src/pages/Profile/Component/ProfileChild.jsx
@@ -1,88 +1,96 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { unfollowUser, followUser } from '../../../store/userSlice'
-import '../Profile.css'
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { unfollowUser, followUser } from "../../../store/userSlice";
+import "../Profile.css";
 
-export const ProfileChild = ({userProfile}) => {
-   const {user, token} = useSelector((store)=>store.authenticate)
-   const {users} = useSelector((store)=>store.users)
-   const dispatch = useDispatch()
-   
-   const findUser = (arr)=>{
-    return arr.length!==0?  arr.find((userCheck)=>userCheck.username === user.username).following : []
-  }
+export const ProfileChild = ({ userProfile }) => {
+  const { user, token } = useSelector((store) => store.authenticate);
+  const { users } = useSelector((store) => store.users);
+  const dispatch = useDispatch();
 
-  const checkUser = (arr)=>{
-    return arr.some((checkUserItem)=>checkUserItem.username===userProfile.username)
-  }
+  const findUser = (arr) => {
+    return arr.length !== 0
+      ? arr.find((userCheck) => userCheck.username === user.username).following
+      : [];
+  };
 
-  const unfollowHandler = ()=>{
-    dispatch(unfollowUser({followUserId: userProfile._id, encodedToken: token}))
-   }
+  const checkUser = (arr) => {
+    return arr.some(
+      (checkUserItem) => checkUserItem.username === userProfile.username
+    );
+  };
 
-   const followHandler = ()=>{
-    dispatch(followUser({followUserId: userProfile._id, encodedToken: token}))
-   }
+  const unfollowHandler = () => {
+    dispatch(
+      unfollowUser({ followUserId: userProfile._id, encodedToken: token })
+    );
+  };
 
-  let avatar = users.find((userItem)=>userItem.username===userProfile.username)?.avatar
-  
+  const followHandler = () => {
+    dispatch(
+      followUser({ followUserId: userProfile._id, encodedToken: token })
+    );
+  };
+
+  let avatar = users.find(
+    (userItem) => userItem.username === userProfile.username
+  )?.avatar;
+
   return (
     <div>
-          {/* profile-card */}
-          <div className="profile-container">
-                <div className="cover-photo"></div>
-                <div className="avatar-round profile-avatar ht-100 wd-100">
-                    <img className="avatar-image" src={`${avatar}`}/>
-                </div>
-                <div className="profile-content">
-                <div className="profile-header">
-                    <div className="profile-header-left">
-                       
-                        <div className="profile-username-container">
-                            <span>{`${userProfile?.firstName} ${userProfile?.lastName}`}</span>
-                            <small>{`@${userProfile?.username}`}</small>
-                        </div>
-                    </div>
-                    <div className="profile-header-right">
-                        <button className='profile-cta-btn'>
-                            Edit Profile
-                        </button>
-                    </div>
-                </div>
-                <div className="profile-body">
-                    <div className="profile-bio">
-                        {userProfile.bio}
-                    </div>
-                    <div className="profile-info">
-                        <a href={`${userProfile.website}`}>{userProfile.website}</a>
-                        <div className='profile-info-child'><span className='material-icons'>calendar_month</span> Joined August 2020</div>
-                    </div>
-                </div>
-                <div className="profile-footer">
-                    <span>10 Followers</span>
-                    <span>5 Following</span>
-                    {
-                        userProfile.username !== user.username &&
-                        <>
-                        {checkUser(findUser(users)) &&
-                             <button onClick={unfollowHandler} className='profile-cta-btn'>
-                             unfollow
-                            </button>
-                        }
-                        {!checkUser(findUser(users)) &&
-                             <button onClick={followHandler} className='profile-cta-btn'>
-                             follow
-                            </button>
-                        }
-                        </>
-                   
-                    }
-                   
-                </div>
-                </div>
+      {/* profile-card */}
+      <div className="profile-container">
+        <div className="cover-photo"></div>
+        <div className="avatar-round profile-avatar ht-100 wd-100">
+          <img className="avatar-image" src={`${avatar}`} />
+        </div>
+        <div className="profile-content">
+          <div className="profile-header">
+            <div className="profile-header-left">
+              <div className="profile-username-container">
+                <span>{`${userProfile?.firstName} ${userProfile?.lastName}`}</span>
+                <small>{`@${userProfile?.username}`}</small>
+              </div>
             </div>
+            <div className="profile-header-right">
+              <button className="profile-cta-btn">Edit Profile</button>
+            </div>
+          </div>
+          <div className="profile-body">
+            <div className="profile-bio">{userProfile.bio}</div>
+            <div className="profile-info">
+              <div className="profile-info-child-flex">
+                <a href={`${userProfile.website}`}>{userProfile.website}</a>
+              </div>
+
+              <div className="profile-info-child">
+                <span className="material-icons">calendar_month</span> Joined
+                August 2020
+              </div>
+            </div>
+          </div>
+          <div className="profile-footer">
+            <span>10 Followers</span>
+            <span>5 Following</span>
+            {userProfile.username !== user.username && (
+              <>
+                {checkUser(findUser(users)) && (
+                  <button onClick={unfollowHandler} className="profile-cta-btn">
+                    unfollow
+                  </button>
+                )}
+                {!checkUser(findUser(users)) && (
+                  <button onClick={followHandler} className="profile-cta-btn">
+                    follow
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
